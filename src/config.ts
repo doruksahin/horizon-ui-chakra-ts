dd7b2ddf151518d8f5d634bc7211b4116340716b
doruk <doruksahin98@gmail.com>
2023-11-11 20:42:49 +0300

    cra to vite

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
-export const API_URL = process.env.REACT_APP_API_URL;
-export const SERVER_URL = process.env.REACT_APP_SERVER_URL
+export const API_URL = import.meta.env.VITE_API_URL
+export const SERVER_URL = import.meta.env.VITE_SERVER_URL
